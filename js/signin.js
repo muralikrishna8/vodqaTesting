c4761746ad822c4dab0ca8dad8ef4ff850d4f835
Murali Krishna <vmurali1444@gmail.com>
2016-10-04 19:14:43 +0530

    [Murali/Gopi] - created login page

---
+++ js/signin.js
@@ -0,0 +1,14 @@
+function validate() {
+  var username = document.getElementById("userName").value;
+  var password = document.getElementById("password").value;
+
+  if(username === "admin" && password === "admin") {
+    window.location.href = "blog.html";
+  } else {
+    var $errMsg = document.getElementById("errorMessage");
+    $errMsg.style.display = "block";
+    $errMsg.innerHTML = "Wrong username or password!"
+  }
+
+  return false;
+}
