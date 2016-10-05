b964421bffb2a1d1ec7a71c12fdbbc2a3239195e
Murali Krishna <vmurali1444@gmail.com>
2016-10-05 21:07:02 +0530

    [Murali/Gopi] - created blog html template

---
+++ js/blog.js
@@ -0,0 +1,9 @@
+function validate() {
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var blogArea = document.getElementById("blogArea").value;
+}
+
+function resetting() {
+  console.log("resetting")
+}
