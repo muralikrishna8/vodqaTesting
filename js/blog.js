94575fdcf6f7eaeb0325be1e5a28bd8a65ddcf0c
Murali Krishna <vmurali1444@gmail.com>
2016-10-05 21:52:58 +0530

    [Murali] - changed the profession to select

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,18 @@
+function professionSelected(select) {
+  if(select.value === "other") {
+    $("#otherProfession").show();
+  } else {
+    $("#otherProfession").hide();
+  }
+};
+
 function validate() {
   var name = document.getElementById("name").value;
   var age = document.getElementById("age").value;
+  var experience = $('input[name=experience]:checked').val();
   var blogArea = document.getElementById("blogArea").value;
 }
 
 function resetting() {
-  var $otherProfession = $("#otherChk");
-  if($otherProfession.prop('checked')) {
-    setTimeout(function() {
-      $otherProfession.prop('checked', true);
-    }, 50);
-  }
+  
 }
